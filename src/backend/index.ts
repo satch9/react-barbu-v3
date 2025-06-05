1b6501d0a647cee7dd11146e7fe1cc7a32f4f6b7
Vincent Jue <viny1976@gmail.com>
2025-06-05 22:35:39 +0400

    Remove redundant CORS configuration

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -34,22 +34,6 @@ app.use((req, res, next) => {
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-/** Rules of our API */
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-
-    if (req.method == "OPTIONS") {
-        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-        return res.status(200).json({});
-    }
-
-    next();
-});
-
 /** Healthcheck */
 app.get("/ping", (_req, res) => {
     return res.status(200).json({ hello: "world!" });
