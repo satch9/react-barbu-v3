683823bc4e923f6fcfeb026447b22dc0fbc69fca
Vincent Jue <viny1976@gmail.com>
2025-06-05 22:29:54 +0400

    Fix WebSocket Connection Issue

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -2,10 +2,7 @@ import { useEffect, useRef } from "react";
 import io, { ManagerOptions, SocketOptions, Socket } from "socket.io-client";
 
 export const useSocket = (uri: string, opts?: Partial<ManagerOptions & SocketOptions> | undefined): Socket => {
-    const { current: socket } = useRef(io(uri, {
-        ...opts,
-        transports: ['websocket']
-    }));
+    const { current: socket } = useRef(io(uri, opts));
 
     useEffect(() => {
         return () => {
